8fc040c130c4ed6d9086a015da343d7bdea7a4e8
Matheus Moura <matheus.moura13@gmail.com>
2018-11-18 20:26:09 -0300

    Remove PersistentDrawer

--- src/components/Layout/PersistentDrawer.tsx
+++
@@ -1,8 +0,0 @@
-import Drawer from "@material-ui/core/Drawer";
-import React, { Component } from "react";
-
-export default class PersistentDrawer extends Component {
-  public render() {
-    return <Drawer variant="persistent" />;
-  }
-}
