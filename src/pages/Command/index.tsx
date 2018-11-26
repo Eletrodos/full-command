1f07d890c15ba21b79bfa01d16b2d9b0efc1b677
Matheus Moura <matheus.moura13@gmail.com>
2018-11-26 20:59:56 -0300

    Adiciona fetch de arquivos e renderiza o json na tela

--- src/pages/Command/index.tsx
+++ src/pages/Command/index.tsx
@@ -6,13 +6,35 @@ interface ICommandProps {
   match: any;
 }
 
-export default class Command extends Component<ICommandProps> {
-  public render() {
+interface ICommandStates {
+  data: any;
+}
+
+export default class Command extends Component<ICommandProps, ICommandStates> {
+  public state = {
+    data: { loading: true }
+  };
+
+  constructor(props: ICommandProps) {
+    super(props);
     const { match } = this.props;
     const { params } = match;
+    // Pega os dados do arquivo na pasta data
+    this.getFileData(params.command).then(data => {
+      this.setState({ data });
+    });
+  }
+  public async getFileData(command: string) {
+    const response = await fetch(`/data/${command}.json`);
+    const data = await response.json();
+    return data;
+  }
+
+  public render() {
+    const { data } = this.state;
     return (
       <Layout title="Full Command">
-        Commando a ser carregado: <b>{params.command}</b>
+        <pre>{JSON.stringify(data, null, "\n")}</pre>
       </Layout>
     );
   }
