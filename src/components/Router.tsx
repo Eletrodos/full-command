e052edc837c28d74e618f6935d49b0b1cfd493d6
Matheus Moura <matheus.moura13@gmail.com>
2018-11-26 19:51:58 -0300

    Cria página vazia para Home

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Command from "../pages/Command";
+import Home from "../pages/Home";
 
 export default class Router extends Component {
   public render() {
     return (
-      <div>
-        <BrowserRouter>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={Home} />
           <Route path="/command/:command" exact component={Command} />
-        </BrowserRouter>
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import Layout from "../../components/Layout";
+
+export default class Home extends Component {
+  public render() {
+    return (
+      <Layout title="Full Command">
+        Bem vindo ao full command, este projeto está em construção.
+      </Layout>
+    );
+  }
+}
